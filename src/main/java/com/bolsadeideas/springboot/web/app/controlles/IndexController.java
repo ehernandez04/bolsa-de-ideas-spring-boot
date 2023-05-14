0c016ddbd884940e567524d70002bcd93f698d80
Erick Hernández <erickhernandez0492@gmail.com>
2023-05-14 20:42:32 +0200

    usando lista

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controlles/IndexController.java
@@ -0,0 +1,57 @@
+package com.bolsadeideas.springboot.web.app.controlles;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app") //ruta de primer nivel
+public class IndexController {
+
+    @GetMapping ({"/index", "/","", "/home"}) //ruta de segundo nivel
+    public String index(Model model){
+        model.addAttribute("titulo", "Hola Spring Framework con Model");
+        return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellido("Guzmán");
+        usuario.setEmail("andres@correo.com");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: " .concat(usuario.getNombre()));
+
+        return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model){
+
+        model.addAttribute("titulo", "Listado de usuarios");
+
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios(){
+
+        List<Usuario> usuarios = Arrays.asList(new Usuario("Andrés", "Guzmán", "andres@correo.com"),
+                new Usuario("John","Doe", "john@correo.com"),
+                new Usuario("Jane","Doe", "jane@correo.com"),
+                new Usuario("Tornado","Roe", "roe@correo.com"));
+
+        return usuarios;
+    }
+
+}
