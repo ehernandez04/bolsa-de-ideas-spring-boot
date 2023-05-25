d51e050af03ffb28c7eed06aef9d48aa4acb6154
Erick Hernández <erickhernandez0492@gmail.com>
2023-05-25 12:10:14 +0200

    Obtener parámetros de la URL con @RequestParam

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,30 @@ public class EjemploParamsController {
     public String param(@RequestParam(name = "texto", required = false, defaultValue = "algún valor...") String texto, Model model){
 
         model.addAttribute("resultado", "El texto enviado es: " + texto);
+
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+
+        model.addAttribute("resultado", "El texto enviado es: " + saludo + "' y el número es '" + numero + "'");
+
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){
+
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+             numero = Integer.parseInt(request.getParameter("numero"));
+        }catch (NumberFormatException e){
+            numero = 0;
+        }
+
+        model.addAttribute("resultado", "El saludo enviado es: " + saludo + "' y el número es '" + numero + "'");
 
         return "params/ver";
     }
